e6afef2544c5144d577d992526be7e63b165ba64
Yan6425 <yan.hourdebaigt.hiroux@gmail.com>
2024-10-15 16:07:01 +0200

    solution

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct Maillon {
+    int donnee;
+    struct Maillon* suivant;
+} Maillon;
+
+typedef Maillon* Liste;
+
+Liste creerListe(int valeur) {
+    Liste liste = malloc(sizeof(Maillon));
+    liste->donnee = valeur;
+    return liste;
+}
+
+Liste ajouterValeur(Liste liste, int valeur) {
+    if (liste == NULL) {
+        return creerListe(valeur);
+    }
+    else {
+        liste->suivant = ajouterValeur(liste->suivant, valeur);
+        return liste;
+    }
+}
+
+int lireListe(Liste liste, int i){
+    if (i) {
+        return lireListe(liste->suivant, i - 1);
+    }
+    else if (liste == NULL) {
+        printf("Erreur d'indice\n");
+        assert(0);
+    }  
+    else {
+        return liste->donnee;
+    }
+}
+
+int main(int argc, char** argv){
+    if (argc < 3 || atoi(argv[1]) < 0) {
+        printf("Erreur d'indice\n");
+        return EXIT_FAILURE;
+    }
+    Liste liste = creerListe(atoi(argv[2]));
+    for (int i = 3; i < argc; i++) {
+        liste = ajouterValeur(liste, atoi(argv[i]));
+    }
+    printf("%d\n", lireListe(liste, atoi(argv[1])));
+
+    return EXIT_SUCCESS;
+}
